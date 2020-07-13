61ab14a3cba72244f98976a8a6f0cab482429142
mixass <67761405+mixass@users.noreply.github.com>
2020-07-14 00:32:03 +0300

    Update wd.lua

--- wd.lua
+++ wd.lua
@@ -1,36 +1,23 @@
 Citizen.CreateThread(function()
     while true do
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_COMBATPISTOL"), 0.45)
-    	Wait(0)
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SNSPISTOL"), 0.35)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_CARBINERIFLE"), 0.22)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_MICROSMG"), 0.25)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_ASSAULTRIFLE"), 0.70)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_GUSENBERG"), 0.55)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_REVOLVER"), 0.55)
-    	Wait(0)	
-	    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SAWNOFFSHOTGUN"), 0.05)
-    	Wait(0)	
-		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SWITCHBLADE"), 0.55)
-    	Wait(0)	
+        Citizen.Wait(3)
+        SetWeaponDamageModifier(0xA2719263, 0.2) -- UNARMED
+        SetWeaponDamageModifier(0xD8DF3C3C, 0.2) -- KNUCKLE
+        SetWeaponDamageModifier(0x1B06D571, 0.35) -- PISTOL
+        SetWeaponDamageModifier(0xBFD21232, 0.35) -- SNSPISTOL
+        SetWeaponDamageModifier(0x5EF9FEC4, 0.4) -- COMBATPISTOL
+        SetWeaponDamageModifier(0x97EA20B8, 0.4) -- DOUBLEACTION
+        SetWeaponDamageModifier(0x678B81B1, 0.1) -- NIGHTSTICK
+        SetWeaponDamageModifier(0x4E875F73, 0.4) --HAMMER
+        SetWeaponDamageModifier(0x958A4A8F, 0.4) -- BAT
+        SetWeaponDamageModifier(0x440E4788, 0.4) -- GOLFCLUB
+        SetWeaponDamageModifier(0x84BD7BFD, 0.4) -- CROWBAR
+        SetWeaponDamageModifier(0xF9E6AA4B, 0.4) -- BOTTLE
+        SetWeaponDamageModifier(0x92A27487, 0.4) -- DAGGER
+        SetWeaponDamageModifier(0xF9DCBF2D, 0.5) -- HATCHET
+        SetWeaponDamageModifier(0xDD5DF8D9, 0.4) -- MACHETE
+        SetWeaponDamageModifier(0x8BB05FD7, 0.1) -- FLASHLIGHT
+        -- SetWeaponDamageModifier(-1553120962, 0.3)-- araç ile ezme
     end
 end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-	local ped = PlayerPedId()
-        if IsPedArmed(ped, 6) then
-	   DisableControlAction(1, 140, true)
-       	   DisableControlAction(1, 141, true)
-           DisableControlAction(1, 142, true)
-        end
-    end
-end)
-
 ---Discord : MIXAS#1234
